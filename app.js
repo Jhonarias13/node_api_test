e3790afc368066a37acb26cca7bfc40a12500250
Jhon Freiman Arias <jhonarias13@Jhons-MacBook-Pro.local>
2021-07-30 17:00:11 -0500

    cambios

--- app.js
+++ app.js
@@ -2,14 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const morgan = require('morgan')
+// const morgan = require('morgan')
 const PORT = process.env.PORT || 3000;
 const { dbConnect }  = require('./config/mongo');
 
 app.use(cors());
 app.use(express.json());
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 app.use('/api/1.0/', require('./app/routes'));
 
 dbConnect();
